52651dbfdb383aa75ff705ffcdba15776e937318
kazuyuki-mera <49195547+kazuyuki-mera@users.noreply.github.com>
2021-01-16 23:06:06 +0900

    crate class component and functional component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,34 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class App extends Component {
-  render() {
-    return (
-      <>
-        <label htmlFor="bar">bar</label>
-        <input
-          id="bar"
-          type="text"
-          onChange={() => {
-            console.log('I am Changed.');
-          }}
-        />
-      </>
-    );
-  }
-}
+// class App extends Component {
+//   render() {
+//     return (
+//       <>
+//         <label htmlFor="bar">bar</label>
+//         <input
+//           id="bar"
+//           type="text"
+//           onChange={() => {
+//             console.log('I am Changed.');
+//           }}
+//         />
+//       </>
+//     );
+//   }
+// }
+
+const App = () => {
+  return (
+    <div>
+      <Cat />
+      <Cat />
+      <Cat />
+    </div>
+  );
+};
+
+const Cat = () => {
+  return <div>Meaow!</div>;
+};
 
 export default App;
